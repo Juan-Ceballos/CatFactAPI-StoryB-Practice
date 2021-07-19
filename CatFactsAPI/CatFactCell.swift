035deb8207cac4a3648691e501e08c998943a354
Juan C <juanceballos@pursuit.org>
2021-07-19 19:26:07 -0400

    refactoring storyboard collection view, holds 3 facts then refreshes to replace oldest fact with new

---
+++ CatFactsAPI/CatFactCell.swift
@@ -0,0 +1,12 @@
+//
+//  CatFactCell.swift
+//  CatFactsAPI
+//
+//  Created by Juan Ceballos on 7/19/21.
+//
+
+import UIKit
+
+class CatFactCell: UICollectionViewCell {
+    
+}
--- CatFactsAPI/ViewController.swift
+++ CatFactsAPI/ViewController.swift
@@ -9,25 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var catLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var catFacts = [CatFact]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
     }
+    
+    
+    
+    
+}
 
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        CatFactAPI.fetchCatFact { (result) in
-            switch result {
-            case .failure(let error):
-                print(error)
-            case .success(let catFact):
-                DispatchQueue.main.async {
-                    self.catLabel.text = catFact.fact
-                }
-            }
-        }
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        0
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "catFact", for: indexPath) as? CatFactCell else {
+            fatalError("Could not load reuse cell datasource")
+        }
+        
+        
+        
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
     
 }
 
